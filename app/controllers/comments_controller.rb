42c60533dc97f746b2a16f1616c551dc4baaa618
Igor T <maniacrush@gmail.com>
2020-02-06 20:20:21 +0200

    add a coment related to post and author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,27 @@
 class CommentsController < InheritedResources::Base
 
+  def new
+    @comment = current_user.comments.new(comment_params)
+  end
+  
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      ;flash[:notice] = 'Comment was successfully created.'
+      redirect_to(@comment.post)
+    else
+      flash[:notice] = "Error creating comment: #{@comment.errors}"
+      redirect_to(@comment.post)
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to(@comment.post)
+  end
+
   private
 
     def comment_params
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
+    validates_presence_of :content
+    validates_length_of :content, :within => 1..100
 end
